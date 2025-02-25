d763e8dccea25df98d43735d451a8c4896898050
Kareem Nasrallah <kareemnasrallah88@gmail.com>
2025-02-25 22:07:06 +0200

    work on products api

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,22 @@
+import Banner from "@/Components/Banner";
+import Products from "@/Components/Products";
 
-export default function Home() {
+export default function Home({ productData }) {
+  console.log(productData);
   return (
     <>
       <main>
-        <div className="py-10">this is the Home Page</div>
+        <div className="max-w-screen-2xl mx-auto">
+          <Banner />
+          <Products />
+        </div>
       </main>
     </>
   );
 }
+
+export const getServerSideProps = async () => {
+  const res = await fetch("https://fakestoreapiserver.reactbd.com/tech");
+  const productData = await res.json();
+  return { props: { productData } };
+};
---
+++ type.d.ts
@@ -0,0 +1,4 @@
+interface ProductsType {
+rand:string;
+
+}[]
