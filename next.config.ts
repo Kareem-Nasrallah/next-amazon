1332c6ab4abc21aa7c8ad70e2f360039fc74d305
Kareem Nasrallah <kareemnasrallah88@gmail.com>
2025-03-18 05:36:44 +0200

    solve frist error 2

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ["i.ibb.co"],
+    domains: ["avatars.githubusercontent.com", "i.ibb.co"],
   },
   reactStrictMode: true,
 };
